3a5ef7bf391b25f5f41d018bd8f06ffbc9f691ea
Kevin <kg-diseno@outlook.es>
2021-08-07 03:38:12 -0300

    Remover movimiento en X

--- BodaGame/Assets/Scripts/Player/PlayerController.cs
+++ BodaGame/Assets/Scripts/Player/PlayerController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float movementSpeed;
     public float jumpForce;
     public Rigidbody player;
     
@@ -16,19 +15,14 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        movePlayer();
-
-        if (isJumping()) 
+        if (isJumpingKey()) 
             Jump();
     }
 
-    private void movePlayer() =>
-        player.velocity = new Vector3(Input.GetAxis("Horizontal") * movementSpeed, player.velocity.y,
-            player.velocity.z);
 
     private Vector3 Jump() => 
         player.velocity = new Vector3(player.velocity.x, jumpForce, player.velocity.z);
 
-    private static bool isJumping() => 
+    private static bool isJumpingKey() => 
         Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
 }
